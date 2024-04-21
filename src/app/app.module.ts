4a7d5998435524ee4c136d685fdc4130baa530c8
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-04-21 21:48:49 +0300

    Display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HeroesComponent,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
