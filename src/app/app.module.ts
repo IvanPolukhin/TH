8018175e2d99e80ee7d8a2111680e61d8b303d56
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-04-22 00:24:57 +0300

    Create a feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
