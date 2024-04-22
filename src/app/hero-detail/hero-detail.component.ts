f89c045a65e4c7160c028336a97adab4b5f9187f
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-04-22 16:46:09 +0300

    Add services

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,9 +4,8 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
-
 export class HeroDetailComponent {
   @Input() hero?: Hero;
 }
